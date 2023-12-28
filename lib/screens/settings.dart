9ee2d867d913c583bd514770a757fbfdd8b4f0a6
Sediq] <sediqabdullahi04@gmail.com>
2023-12-29 00:27:12 +0100

    Added an update

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -29,7 +29,7 @@ class _SettingsState extends State<Settings> {
               ),
               IconButton(
                   onPressed: () {},
-                  style: IconButton.styleFrom(backgroundColor: BG_AMBER),
+                  style: IconButton.styleFrom(foregroundColor: BG_AMBER),
                   icon: Icon(
                     Icons.double_arrow,
                     color: Colors.white,
